0b3f24fa5aea89d42dc947ad7b3b0a8d3012a03f
Craig Cook <boycook@me.com>
2013-01-20 13:57:53 +0000

    Got oauth for timelines working

--- test/spec/TwitterSpec.js
+++ test/spec/TwitterSpec.js
@@ -1,15 +1,78 @@
 Twitter = require('../../lib/Twitter').Twitter;
 
-describe('Twitter', function(){
+describe('Twitter', function () {
     var twitter;
+    var error = function (code, data) {
+        console.log('ERROR [%s]', code);
+        done();
+    };
+    var success = function (data) {
+        expect(data.length).toEqual(20);
+        done();
+    };
 
-    beforeEach(function(){
+    beforeEach(function (done) {
         twitter = new Twitter();
         expect(twitter).toBeDefined();
+        expect(twitter.oauth).toBeDefined();
+        done();
     });
 
-    it('should get timeline for a user', function() {
-		//TODO: async assertions
-		twitter.getUserTimeline()
+    it('should get request token', function (done) {
+        twitter.getOAuthRequestToken(function (oauth) {
+            done();
+        });
+    });
+
+    it('should get timeline for a user', function (done) {
+        var params = { screen_name: 'BoyCook', count: '10'};
+        twitter.getUserTimeline(params, error,
+            function (data) {
+                expect(data.length).toEqual(10);
+                done();
+            }
+        );
+    });
+
+    it('should get home timeline', function (done) {
+        var params = { count: '10'};
+        twitter.getHomeTimeline(params, error,
+            function (data) {
+                expect(data.length).toEqual(10);
+                done();
+            }
+        );
+    });
+
+    it('should get mentions timeline', function (done) {
+        var params = { count: '10'};
+        twitter.getMentionsTimeline(params, error,
+            function (data) {
+                expect(data.length).toEqual(10);
+                done();
+            }
+        );
+    });
+
+    it('should get re tweets', function (done) {
+        var params = { count: '10'};
+        twitter.getReTweetsOfMe(params, error,
+            function (data) {
+                expect(data.length).toEqual(0);
+                done();
+            }
+        );
+    });
+
+    it('should get tweet by id', function (done) {
+        var data = { id: '292964063091236860'};
+        var params = { id: '292964063091236864'};
+        twitter.getTweet(params, error,
+            function (tweet) {
+                expect(tweet.id_str).toEqual(params.id);
+//                expect(tweet.id).toEqual(data.id);
+                done();
+            }
+        );
     });
 });
